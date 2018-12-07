b709950cac213498c3088a402a43135c418dba6c
JoeMuff999 <joeymuffoletto10@gmail.com>
2018-12-07 12:23:26 -0600

    Final Commits

--- trick_sims/FirstSim/RUN_test/input.py
+++ trick_sims/FirstSim/RUN_test/input.py
@@ -3,7 +3,7 @@ drg = trick.DRAscii("rocket") ;
 drg.add_variable("dyn.rocket.pos[0]", "x_pos")
 drg.add_variable("dyn.rocket.pos[1]", "y_pos")
 drg.add_variable("dyn.rocket.angle", "angle")
-drg.set_cycle(0.1)
+drg.set_cycle(0.0166666)
 
 trick.add_data_record_group(drg, trick.DR_Buffer)
-execfile("Modified_data/realtime.py")
+#execfile("Modified_data/realtime.py")
